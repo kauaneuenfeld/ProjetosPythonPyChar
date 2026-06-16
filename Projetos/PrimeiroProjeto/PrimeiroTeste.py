455f36d6b8fd10b2a50257d01eff8314bf938a1a
kauaneuenfeld <kauaneuenfeld@gmail.com>
2026-06-16 08:22:18 -0300

    Create PrimeiroTeste.py

---
+++ Projetos/PrimeiroProjeto/PrimeiroTeste.py
@@ -0,0 +1,35 @@
+idade = 18
+altura = 1.75
+nome = "João"
+aprovado = True
+frutas = ["Maçã", "Banana", "Uva"]
+cores = ("azul", "Verde", "vermelho")
+
+aluno = {
+    "nome": "Maria",
+    "idade": 18,
+    "curso": "Desenvolvimento d esistemas"
+}
+
+print("Exemplos de tipos de dados em Python\n")
+
+print("Idade=", idade)
+print("Tipo:", type(idade))
+
+print("\naltura= ", altura)
+print("Tipo:", type(altura))
+
+print("\nNome=", nome)
+print("Tipo:", type(nome))
+
+print("\nAprovado=", aprovado)
+print("Tipo:", type(aprovado))
+
+print("\nFrutas=", frutas)
+print("Tipo:", type(frutas))
+
+print("\nCores=", cores)
+print("Tipo:", type(cores))
+
+print("\nAluno=", aluno)
+print("Tipo:", type(aluno))
